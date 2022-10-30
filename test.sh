20b508df168ca1234c04e7de1a0412d045f1140d
arctich <karuto.no@proton.me>
2022-10-30 17:56:23 +0000

    removed some files

--- test.sh
+++
@@ -1,4 +0,0 @@
-# this repeats character a 19 times then hashes it into a md5
-# (v)erbose (b)ruteforce (H)ash (a)lphabet [a, b]
-# 8.243s
-time cargo run -- -vH ${$(echo -n $(seq -s a 20 | tr -d '[:digit:]') | md5sum)::-1} -ba ab
